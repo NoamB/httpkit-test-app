8d6e70362c38e653760d5aba194539476929d2ef
Noam Ben Ari <noam.ben.ari@cloudon.com>
2013-09-14 13:29:21 +0300

    Adding static file from /public.

--- src/httpkit/core.clj
+++ src/httpkit/core.clj
@@ -3,15 +3,32 @@
   (:require [clojure.tools.logging :refer [info error]])
   (:require [ring.middleware.reload :as reload]
             [compojure.core :refer [defroutes GET POST]]
-            [compojure.route :as route :refer [files not-found]]
+            [compojure.route :as route :refer [resources files not-found]]
             [compojure.handler :refer [site]] ; form, query params decode; cookie; session, etc
             [cheshire.core :as json :refer [generate-string]])
   (:gen-class))
 
+(defn- authenticate
+  "Looks in user storage for a user record with the supplied credentials. If found returns true, else false."
+  [username password]
+  (and (= username "noam")
+       (= password "1234"))
+  )
+
 (defn show-landing-page [req]
   {:status  200
    :headers {"Content-Type" "application/json"}
-   :body    (json/generate-string {:foo "barrr" :baz 5})})
+   :body    (json/generate-string {:foo "bar" :baz 5})})
+
+(defn create-login [{params :form-params}]
+  (info params)
+  (if (authenticate (params "username") (params "password"))
+    {:status  200
+     :headers {"Content-Type" "text/html"}
+     :body    (str "you posted login! params: " params) }
+    {:status 403
+     :headers {}
+     :body "wrong credentials!" }))
 
 (defn wrap-logging [handler]
   (fn [req]
@@ -20,10 +37,11 @@
       (info "Sent Response: " \newline resp)
       (info "---REQUEST ENDED---" \newline)
       resp)))
-
+1
 (defroutes all-routes
   (GET "/" [] show-landing-page)
-  (route/files "/static/") ;; static file url prefix /static, in `public` folder
+  (POST "/login" [] create-login)
+  (route/files "/") ;; static file url prefix /, in `public` folder
   (route/not-found "<p>Page not found.</p>")) ;; all other, return 404
 
 (defn in-dev?
@@ -37,7 +55,10 @@
   ;;(alter-var-root #'*read-eval* (constantly false))
   (info "starting up ...")
   (info "args: " args)
-  (let [handler (if (in-dev? args)
-                  (reload/wrap-reload (site #'all-routes)) ;; only reload when dev
-                  (site all-routes))]
-    (run-server (-> handler (wrap-logging)) {:port 8080})))
+  (let [dev-mode (in-dev? args) handler (if dev-mode
+                                          (reload/wrap-reload (site #'all-routes)) ;; only reload when dev
+                                          (site all-routes))]
+    (if dev-mode
+      (info "--- DEV MODE ---")
+      (info "PROD MODE"))
+    (run-server (-> handler wrap-logging) {:port 8080})))
