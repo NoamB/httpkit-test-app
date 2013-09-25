426f6f28353ec1f5a74c3c32aeeb8e7951bfb6bc
Noam Ben Ari <noam.ben.ari@cloudon.com>
2013-09-25 22:17:16 +0300

    setting up tools.logging with slf4j and log4j

--- project.clj
+++ project.clj
@@ -10,10 +10,8 @@
                  [http-kit "2.1.10"]
                  [compojure "1.1.5"]
                  [cheshire "5.2.0"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-api "1.7.5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.clojure/tools.logging "0.2.6"]
                  [clj-bcrypt-wrapper "0.1.0"]]
   :main core)
