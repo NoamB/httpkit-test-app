9decacbc91a51efed2118e44c13c4238f80bf6d3
Noam Ben Ari <noam.ben.ari@cloudon.com>
2013-10-19 00:26:07 +0300

    Looping over *authentication-methods* until user is authenticated

--- src/noam/controller.clj
+++ src/noam/controller.clj
@@ -4,7 +4,7 @@
             [compojure.response :refer [render]]
             [compojure.core :refer [routes GET POST]]
             [compojure.route :as route :refer [resources files not-found]]
-            [noamb.foe.auth :refer [logged-in? authenticate login-session reset-session]]
+            [noamb.foe.auth :refer [logged-in? authenticate login logout]]
             [noamb.foe.user :refer [find-user]]))
 
 (defn not-authenticated
@@ -14,23 +14,25 @@
    :body "wrong credentials!"})
 
 (defn index [{session :session :as req}
-             subsystem]
+             {user-storage :user-storage :as subsystem}]
   (if (logged-in? session)
-    (let [user (find-user (:user-storage subsystem) {:id (session :user-id)})]
+    (let [identifiers {:id (session :user-id)}
+          user (find-user user-storage identifiers)]
       (render (str "Welcome " (:username user) "! <a href=\"/logout\">Logout</a>") req))
     (redirect "/login.html")))
 
-(defn login [{session :session params :form-params :as req}
-             {db :user-storage :as subsystem}]
+(defn create-session [{session :session
+                       params :form-params :as req}
+                      {db :user-storage :as subsystem}]
   (if-let [user (authenticate db {:username (params "username")
                                   :password (params "password")})] ; TODO: sanitize user data?
-    (let [session (login-session session (:id user))]
+    (let [session (login session (:id user))]
       (assoc (redirect "/") :session session))
     (not-authenticated)))
 
-(defn logout
+(defn destroy-session
   [req]
-  (reset-session (redirect "/")))
+  (logout (redirect "/")))
 
 (defn myjson
   [req id]
@@ -40,8 +42,8 @@
   [system]
   (routes
     (GET "/" [] #(index % (:foe-config system)))
-    (POST "/login" [] #(login % (:foe-config system)))
-    (GET "/logout" [] logout)
+    (POST "/login" [] #(create-session % (:foe-config system)))
+    (GET "/logout" [] destroy-session)
     (GET "/myjson/:id" [id] #(myjson % id))
     (route/files "/") ; static file url prefix /, in `public` folder
     (route/not-found "<p>Page not found.</p>"))) ; all other, return 404
--- src/noamb/foe/auth.clj
+++ src/noamb/foe/auth.clj
@@ -3,22 +3,28 @@
     :doc "Functions for updating the session with the authentication state."}
   (:require [noamb.foe.user :refer [authenticate-from-storage]]))
 
+(def ^{:dynamic true} *authentication-methods* [authenticate-from-storage])
+
 (defn logged-in?
   "Takes a session map and inspects it for the required keys to be logged in.
    Returns true if logged in, false otherwise."
   [session]
   (boolean (session :user-id)))
 
-(defn authenticate ; TODO: should loop over all authenticate options until authenticated
+(defn authenticate
+  "Loops over all authentication methods until authenticated or all methods return nil."
   [db identifiers]
-  (authenticate-from-storage db identifiers))
+  (loop [methods *authentication-methods*]
+    (when (seq methods)
+      (let [user ((first methods) db identifiers)]
+        user))))
 
-(defn login-session
+(defn login
   "Sets user-id in session (effectively logs in the user)."
   [session user-id]
   (assoc session :user-id user-id))
 
-(defn reset-session
+(defn logout
   "Resets session by associng nil to the :session key in the response and thus activating the wrap-session middleware own reset session mechanism."
   [resp]
   (assoc resp :session nil))
